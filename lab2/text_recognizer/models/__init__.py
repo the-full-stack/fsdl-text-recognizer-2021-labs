e61c548a8e800ab666c73792bcf1f532da916957
Sergey Karayev <sergeykarayev@gmail.com>
2021-01-25 19:00:56 -0800

    fix

--- lab2/text_recognizer/models/__init__.py
+++ lab2/text_recognizer/models/__init__.py
@@ -2,6 +2,8 @@ from .mlp import MLP
 
 # Hide lines below until Lab 2
 from .cnn import CNN
+from .line_reshape_cnn import LineReshapeCNN
+from .line_cnn import LineCNN
 
 # Hide lines above until Lab 2
 
